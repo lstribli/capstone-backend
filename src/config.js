bf45cbb1b09d2aab75a50a026e073c14a3368c6d
Logan Stribling <logan.stribling@gmail.com>
2020-05-20 18:45:39 -0600

    setting up for deployment

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://aura@localhost/aura',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://aura@localhost/aura',
   JWT_SECRET: process.env.JWT_SECRET
 };
